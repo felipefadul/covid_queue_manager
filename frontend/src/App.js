b6e8a08b7e28669111cac65d8bde576f3998b3cc
Felipe Fadul <felipe.fadul@hotmail.com>
2021-01-20 22:24:59 -0300

    feat: Cria componente de Rotas

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
+import React from 'react';
+import Routes from "./routes";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Hello, World!
-        </p>
-      </header>
-    </div>
+    <Routes />
   );
 }
 
