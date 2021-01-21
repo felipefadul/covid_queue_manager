5d53318dd9fd2586ed2a29dd5e5724eb26a796f4
Leonardo <leo.gcs@poli.ufrj.br>
2021-01-21 00:02:54 -0300

    version after getting user information

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,39 @@
-import { AzureAD } from "react-aad-msal";
+import { AzureAD, AuthenticationState} from "react-aad-msal";
 import { authProvider } from "./authProvider";
+import { store } from './reduxStore';
+import React from 'react'
+
 
 function App() {
   return (
-    <AzureAD provider={authProvider} forceLogin={true}>
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Hello, World!
-          </p>
-        </header>
-      </div>
-    </AzureAD>
+  <AzureAD provider={authProvider} forceLogin={true} reduxStore={store}>
+  {
+    ({login, logout, authenticationState, error, accountInfo}) => {
+      switch (authenticationState) {
+        case AuthenticationState.Authenticated:
+          return (
+            <p>
+              <span>Welcome, {console.log(accountInfo)} !</span>
+              <button onClick={logout}>Logout</button>
+            </p>
+          );
+        case AuthenticationState.Unauthenticated:
+          return (
+            <div>
+              {error && <p><span>An error occurred during authentication, please try again!</span></p>}
+              <p>
+                <span>Hey stranger, you look new!</span>
+                <button onClick={login}>Login</button>
+              </p>
+            </div>
+          );
+        case AuthenticationState.InProgress:
+          return (<p>Authenticating...</p>);
+      }
+    }
+  }
+</AzureAD>
+ 
   );
 }
 
