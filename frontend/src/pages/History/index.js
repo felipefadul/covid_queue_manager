8130e4bf18189d45199a69116bb50a873ce888c8
Felipe Fadul <felipe.fadul@hotmail.com>
2021-01-25 22:59:40 -0300

    feat: Cria botões da página de Histórico

--- frontend/src/pages/History/index.js
+++ frontend/src/pages/History/index.js
@@ -5,10 +5,23 @@ import GlobalStyle from '../../styles/global';
 
 import { authProvider } from "../../authProvider";
 export default function History() {
-  
+
   return (
     <S.HistoryContainer>
-      Bem-vindo ao Histórico, {authProvider.getAccountInfo()?.account.name}!
+      <S.AppTitle>
+        Sistema de Triagem - Coronavírus
+      </S.AppTitle>
+      <S.Content>
+        Bem-vindo ao Histórico, {authProvider.getAccountInfo()?.account.name}!
+        <S.ButtonArea>
+          <S.Button>
+            CHAMAR PRÓXIMO PACIENTE
+          </S.Button>
+          <S.Button>
+            REALIZAR TRIAGEM
+          </S.Button>
+        </S.ButtonArea>
+      </S.Content>
       <GlobalStyle />
     </S.HistoryContainer>   
   );
--- frontend/src/pages/History/styled.js
+++ frontend/src/pages/History/styled.js
@@ -2,8 +2,70 @@ import styled from 'styled-components';
 
 export const HistoryContainer = styled.div`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   background-color: #E6FFE6;
   height: 100vh;
+`;
+
+export const AppTitle = styled.div`
+  color: #004D00;
+  text-align: center;
+  font-style: normal;
+  font-weight: bold;
+  font-size: 2rem;
+
+  @media(max-width: 800px) {
+    font-size: 1.2rem;
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  background-color: #85E085;
+  border-radius: 1.5rem;
+  padding: 2rem 3rem;
+  margin: 1rem;
+
+  @media(max-width: 800px) {
+    padding: 1.5rem 2.5rem;
+  }
+`;
+
+export const ButtonArea = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media(max-width: 800px) {
+    display: flex;
+    flex-direction: column;
+  }
+`
+
+export const Button = styled.button`
+  height: 3rem;
+  width: 16rem;
+  border: 2px solid #000;
+  background: #008000;
+  font-weight: bold;
+  font-size: 1.1rem;
+  color: #fff;
+  border-radius: 1rem;
+  margin: 1rem .5rem 0 .5rem;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #006600;
+  }
+
+  @media(max-width: 800px) {
+    font-size: .9rem;
+    margin: 0.5rem 0;
+    height: 2.8rem;
+    width: 14rem;
+  }
 `;
