85ab37a09ef0ed60d4ca7f60fb3d624c2dda166a
JoltLeo <leo.gcs@poli.ufrj.br>
2021-03-02 23:46:09 -0300

    adicionar variavel de ambiente para URL do host

--- frontend/src/authProvider.js
+++ frontend/src/authProvider.js
@@ -1,15 +1,27 @@
 // authProvider.js
 import { MsalAuthProvider, LoginType } from "react-aad-msal";
 import { Logger, LogLevel } from "msal";
-// Msal Configurations
+
+import { config } from 'dotenv';
+import { join } from'path';
+import { ok } from 'assert';
+
+const env = process.env.NODE_ENV || "development"; // Por default, é "development".
+ok(env === "production" || env === "development", 'A env é inválida! Deve ser "production" ou "development"');
+
+const configPath = join(__dirname, './config', `.env.${env}`);
+config({
+  path: configPath
+});
+
 export const authProvider = new MsalAuthProvider(
   {
     auth: {
       authority:
         "https://login.microsoftonline.com/ccf4e4a8-f0a6-46f2-80ff-cbe92c1ed943",
       clientId: "d63922d8-e013-4231-9e54-1d8638a05f1b",
-      postLogoutRedirectUri: window.location.origin,
-      redirectUri: "https://localhost:3000/",
+      postLogoutRedirectUri: `https://${process.env.HOST}:3000/`,
+      redirectUri: `https://${process.env.HOST}:3000/`,
       validateAuthority: true,
       // After being redirected to the "redirectUri" page, should user
       // be redirected back to the Url where their login originated from?
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
 
+import { config } from 'dotenv';
+import { join } from'path';
+import { ok } from 'assert';
+
+const env = process.env.NODE_ENV || "development"; // Por default, é "development".
+ok(env === "production" || env === "development", 'A env é inválida! Deve ser "production" ou "development"');
+
+const configPath = join(__dirname, '../config', `.env.${env}`);
+config({
+  path: configPath
+});
+
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: `http://${process.env.HOST}:3333`,
 })
 
 export default api;
