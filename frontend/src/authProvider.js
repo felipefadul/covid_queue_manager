1dccc606e5af6614577126c0c067b2525e212177
JoltLeo <leo.gcs@poli.ufrj.br>
2021-03-03 00:27:13 -0300

    Fix: ajeitar credenciais env do frontend

--- frontend/src/authProvider.js
+++ frontend/src/authProvider.js
@@ -9,10 +9,12 @@ import { ok } from 'assert';
 const env = process.env.NODE_ENV || "development"; // Por default, é "development".
 ok(env === "production" || env === "development", 'A env é inválida! Deve ser "production" ou "development"');
 
-const configPath = join(__dirname, './config', `.env.${env}`);
+const configPath = join(__dirname, '../', `.env.${env}`);
 config({
   path: configPath
 });
+
+console.log('process.env.HOST', process.env.REACT_APP_HOST);
 
 export const authProvider = new MsalAuthProvider(
   {
@@ -20,8 +22,8 @@ export const authProvider = new MsalAuthProvider(
       authority:
         "https://login.microsoftonline.com/ccf4e4a8-f0a6-46f2-80ff-cbe92c1ed943",
       clientId: "d63922d8-e013-4231-9e54-1d8638a05f1b",
-      postLogoutRedirectUri: `https://${process.env.HOST}:3000/`,
-      redirectUri: `https://${process.env.HOST}:3000/`,
+      postLogoutRedirectUri: `https://${process.env.REACT_APP_HOST}:3000/`,
+      redirectUri: `https://${process.env.REACT_APP_HOST}:3000/`,
       validateAuthority: true,
       // After being redirected to the "redirectUri" page, should user
       // be redirected back to the Url where their login originated from?
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -7,13 +7,13 @@ import { ok } from 'assert';
 const env = process.env.NODE_ENV || "development"; // Por default, é "development".
 ok(env === "production" || env === "development", 'A env é inválida! Deve ser "production" ou "development"');
 
-const configPath = join(__dirname, '../config', `.env.${env}`);
+const configPath = join(__dirname, '../../', `.env.${env}`);
 config({
   path: configPath
 });
 
 const api = axios.create({
-  baseURL: `http://${process.env.HOST}:3333`,
+  baseURL: `http://${process.env.REACT_APP_HOST}:3333`,
 })
 
 export default api;
