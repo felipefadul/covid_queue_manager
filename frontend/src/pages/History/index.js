2fe7d7f3574fc9e21a6dbb7426a48bdbacd624b4
Felipe Fadul <felipe.fadul@hotmail.com>
2021-03-02 22:57:59 -0300

    feat: Usuário que não estiver logado visualiza apenas o histórico, sem interação

--- frontend/src/pages/History/index.js
+++ frontend/src/pages/History/index.js
@@ -110,7 +110,8 @@ export default function History() {
   }
 
   async function handleHistorySearch() {
-    await api.get(`/api/historico/consulta/privado`)
+    const visibility = (authenticationState === AuthenticationState.Authenticated) ? 'privado': 'publico';
+    await api.get(`/api/historico/consulta/${visibility}`)
     .then(response => {
       setLastPatient(response.data.historico[0]);
       setHistoryData(response.data.historico.slice(1));
@@ -133,85 +134,88 @@ export default function History() {
   const authenticationState = JSON.parse(localStorage.getItem('authenticationState'));
   const accountListGroups = JSON.parse(localStorage.getItem('accountListGroups'));
   const accountName = localStorage.getItem('accountName');
-  if (authenticationState === AuthenticationState.Authenticated) {
-    return (
-      <S.HistoryContainer>
-        <S.AppTitle>
-          Sistema de Triagem - Coronavírus
-        </S.AppTitle>
-        <S.Content>
-          <S.LastPatientContainer>
-            <S.LastPatientContent>
-              <S.LastPatientContentTitle>
-                Paciente
-              </S.LastPatientContentTitle>
-              <S.LastPatientContentValue>
-                {lastPatient.nome_paciente?.toUpperCase()} <IconButton onClick = { () => handlePatientData(lastPatient.paciente_id) }><SearchIcon/></IconButton>
-              </S.LastPatientContentValue>
-            </S.LastPatientContent>
-            <S.LastPatientContent>
-              <S.LastPatientContentTitle>
-                  Sala
-              </S.LastPatientContentTitle>
-              <S.LastPatientContentValue className="room">
-                {lastPatient.sala_medico}
-              </S.LastPatientContentValue>
-            </S.LastPatientContent>
-          </S.LastPatientContainer>
-          <S.HistoryContent>
-            <S.HistoryContentTitle>
-              Histórico de Chamadas
-            </S.HistoryContentTitle>
-            <ThemeProvider theme={theme}> 
-              <TableContainer component={Paper}>
-                <Table className={classes.table} aria-label="customized table">
-                  <TableHead>
-                    <TableRow>
-                      <StyledTableCell className={classes.columnName} align="center">Paciente</StyledTableCell>
-                      <StyledTableCell className={classes.columnName} align="center">Sala</StyledTableCell>
-                      <StyledTableCell className={classes.columnName} align="center">Horário da Chamada</StyledTableCell>
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {historyData.map((row) => (
-                      <StyledTableRow key={row.paciente_id}>
-                        <StyledTableCell className={classes.rowValue} align="left" component="th" scope="row">
-                          {row.nome_paciente.toUpperCase()}
-                        </StyledTableCell>
-                        <StyledTableCell className={classes.rowValue} align="center">{row.sala_medico}</StyledTableCell>
-                        <StyledTableCell className={classes.rowValue} align="center">{row.data}</StyledTableCell>
-                        <StyledTableCell className={classes.rowValue} align="center"><IconButton onClick = { () => handlePatientData(row.paciente_id) }><SearchIcon/></IconButton></StyledTableCell>
-                      </StyledTableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
-            </ThemeProvider>
-          </S.HistoryContent>
-          <S.ButtonArea>
-            {CheckForValueJson(accountListGroups,'21af395d-6321-4465-9a48-e1aa65178e01') ? <S.Button onClick = { handleCallNextPatient }>CHAMAR PRÓXIMO PACIENTE</S.Button> : ''}
-            {CheckForValueJson(accountListGroups,'77cdb68f-6363-41de-93e8-9e15f2938471') ? <S.Button onClick = { handleScreening }>REALIZAR TRIAGEM</S.Button>:''}
-          </S.ButtonArea>
-          <Snackbar
-            open={openWarningSnackbar}
-            autoHideDuration={5000}
-            onClose={handleCloseWarningSnackbar}>
-            <Alert
-              onClose={handleCloseWarningSnackbar}
-              severity="warning">
-              {`Fila está vazia!`}
-            </Alert>
-          </Snackbar>
-        </S.Content>
-        <GlobalStyle />
-      </S.HistoryContainer>   
-    );
-  }
-  else {
-    return(
-      <Unauthorized />
-    )
-  }
+  return (
+    <S.HistoryContainer>
+      <S.AppTitle>
+        Sistema de Triagem - Coronavírus
+      </S.AppTitle>
+      <S.Content>
+        <S.LastPatientContainer>
+          <S.LastPatientContent>
+            <S.LastPatientContentTitle>
+              Paciente
+            </S.LastPatientContentTitle>
+            <S.LastPatientContentValue>
+              {lastPatient.nome_paciente?.toUpperCase()} 
+              {CheckForValueJson(accountListGroups,'21af395d-6321-4465-9a48-e1aa65178e01') ? 
+                <IconButton onClick = { () => handlePatientData(lastPatient.paciente_id) }>
+                  <SearchIcon/>
+                </IconButton> : ''}
+            </S.LastPatientContentValue>
+          </S.LastPatientContent>
+          <S.LastPatientContent>
+            <S.LastPatientContentTitle>
+                Sala
+            </S.LastPatientContentTitle>
+            <S.LastPatientContentValue className="room">
+              {lastPatient.sala_medico}
+            </S.LastPatientContentValue>
+          </S.LastPatientContent>
+        </S.LastPatientContainer>
+        <S.HistoryContent>
+          <S.HistoryContentTitle>
+            Histórico de Chamadas
+          </S.HistoryContentTitle>
+          <ThemeProvider theme={theme}> 
+            <TableContainer component={Paper}>
+              <Table className={classes.table} aria-label="customized table">
+                <TableHead>
+                  <TableRow>
+                    <StyledTableCell className={classes.columnName} align="center">Paciente</StyledTableCell>
+                    <StyledTableCell className={classes.columnName} align="center">Sala</StyledTableCell>
+                    <StyledTableCell className={classes.columnName} align="center">Horário da Chamada</StyledTableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {historyData.map((row) => (
+                    <StyledTableRow key={row.paciente_id}>
+                      <StyledTableCell className={classes.rowValue} align="left" component="th" scope="row">
+                        {row.nome_paciente.toUpperCase()}
+                      </StyledTableCell>
+                      <StyledTableCell className={classes.rowValue} align="center">{row.sala_medico}</StyledTableCell>
+                      <StyledTableCell className={classes.rowValue} align="center">{row.data}</StyledTableCell>
+                      {CheckForValueJson(accountListGroups,'21af395d-6321-4465-9a48-e1aa65178e01') ? 
+                        <StyledTableCell className={classes.rowValue} align="center">
+                          <IconButton onClick = { () => handlePatientData(row.paciente_id) }>
+                            <SearchIcon/>
+                          </IconButton>
+                        </StyledTableCell> : ''}
+                    </StyledTableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </ThemeProvider>
+        </S.HistoryContent>
+        <S.ButtonArea>
+          {CheckForValueJson(accountListGroups,'21af395d-6321-4465-9a48-e1aa65178e01') ? <S.Button onClick = { handleCallNextPatient }>CHAMAR PRÓXIMO PACIENTE</S.Button> : ''}
+          {CheckForValueJson(accountListGroups,'77cdb68f-6363-41de-93e8-9e15f2938471') ? <S.Button onClick = { handleScreening }>REALIZAR TRIAGEM</S.Button>:''}
+          {(authenticationState !== AuthenticationState.Authenticated) ? <S.Button onClick = { () => history.push('/') } >VOLTAR</S.Button>:''}
+        </S.ButtonArea>
+        <Snackbar
+          open={openWarningSnackbar}
+          autoHideDuration={5000}
+          onClose={handleCloseWarningSnackbar}>
+          <Alert
+            onClose={handleCloseWarningSnackbar}
+            severity="warning">
+            {`Fila está vazia!`}
+          </Alert>
+        </Snackbar>
+      </S.Content>
+      <GlobalStyle />
+    </S.HistoryContainer>   
+  );
 }
 
 //ID grupo Medicos:       21af395d-6321-4465-9a48-e1aa65178e01
